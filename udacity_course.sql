d7db38818f0d740f8a61485d012366ff0bf5286b
ameliascansen <amelia.scansen@gmail.com>
2025-01-02 14:55:40 -0600

    Update udacity_course.sql

--- udacity_course.sql
+++ udacity_course.sql
@@ -21,3 +21,49 @@ SELECT id, account_id,
 poster_amt_usd/(standard_amt_usd + gloss_amt_usd + poster_amt_usd) AS post_per
 FROM orders
 LIMIT 10;
+
+--Logical operators
+--LIKE This allows you to perform operations similar to using WHERE and =, but for cases when you might not know exactly what you are looking for.
+  -- % before or after to represent characters
+--IN This allows you to perform operations similar to using WHERE and =, but for more than one condition.
+--NOT This is used with IN and LIKE to select all of the rows NOT LIKE or NOT IN a certain condition.
+--AND & BETWEEN These allow you to combine operations where all combined conditions must be true.
+--OR This allows you to combine operations where at least one of the combined conditions must be true.
+
+SELECT name
+FROM accounts
+WHERE name LIKE 'C%';
+
+select name
+from accounts
+where name like '%one%';
+
+select name
+from accounts
+where name like '%s';
+
+--IN: allows you to use = but for more than 1 item of that particular column
+select name, primary_poc, sales_rep_id
+from accounts
+where name in ('Walmart', 'Target', 'Nordstrom');
+
+select *
+from web_events
+where channel in ('organic', 'adwords');
+
+--NOT operator: useful operator for working with IN and LIKE, can grab rows that do not meet particular criteria
+select name, primary_poc, sales_rep_id
+from accounts
+where name NOT in ('Walmart', 'Target', 'Nordstrom');
+
+select * 
+from web_events
+where channel NOT IN ('organic', 'adwords');
+
+select name
+from accounts
+where name NOT like ('C%');
+
+SELECT name
+FROM accounts
+WHERE name NOT LIKE '%one%';
