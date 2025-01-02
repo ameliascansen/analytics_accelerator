a906c3712e52fa0a8d9ae4f67956d6c8ae464cbb
ameliascansen <amelia.scansen@gmail.com>
2025-01-02 13:02:18 -0600

    Update udacity_course.sql

--- udacity_course.sql
+++ udacity_course.sql
@@ -7,3 +7,17 @@ limit 10;
 SELECT name, website, primary_poc
 FROM accounts
 WHERE name = 'Exxon Mobil';
+
+--Derived Columns: give name to new column  by using the AS keyword, derived column only exists for duration of query
+SELECT id, (standard_amt_usd/total_amt_usd)*100 AS std_percent, total_amt_usd
+FROM orders
+LIMIT 10;
+
+SELECT id, account_id, standard_amt_usd/standard_qty AS unit_price
+FROM orders
+LIMIT 10;
+
+SELECT id, account_id, 
+poster_amt_usd/(standard_amt_usd + gloss_amt_usd + poster_amt_usd) AS post_per
+FROM orders
+LIMIT 10;
