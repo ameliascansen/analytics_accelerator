276a65eb8d02c3e32d83995db05b0847deade1aa
ameliascansen <amelia.scansen@gmail.com>
2025-01-03 16:48:07 -0600

    Update udacity_course.sql

--- udacity_course.sql
+++ udacity_course.sql
@@ -71,7 +71,88 @@ WHERE name NOT LIKE '%one%';
 --AND operator:used within a WHERE statement to consider more than one logical clause at a time. Each time you link a new statement with an AND, you will need to specify the column you are interested in looking at. 
 --BETWEEN operator:
   -- instead of: WHERE column >= 6 AND column <= 10
-  -- can use: WHERE column BETWEEN 6 AND 10
+  -- can use: WHERE column BETWEEN 6 AND 10 (and will show endpoint values)
+  -- BETWEEN is generally inclusive of endpoints, it assumes the time is at 00:00:00 (i.e. midnight) for dates so for yar 2016 do BETWEEN '2016-01-01' AND '2017-01-01'
+--DESC newest to oldest / alphabetical order
 select standard_qty, poster_qty, gloss_qty
 from orders
 where standard_qty > 1000 AND poster_qty = 0 AND gloss_qty = 0;
+
+SELECT name
+FROM accounts
+WHERE name NOT LIKE 'C%' AND name LIKE '%s';
+
+select occurred_at, gloss_qty
+from orders
+where gloss_qty between 24 and 29;
+
+select id, channel
+from web_events
+where channel in ('organic', 'adwords') and occurred_at between '2016-01-01' AND '2017-01-01'
+order by occured_at desc;
+
+--OR operator: can combine multiple statements, need to specify the column you are interested in looking at.
+  -- if you put parentheses around several logical statements, it will become one large logical statement that will resolve to true or false
+    -- as long as one statement is true, the entire block in parentheses will be considered true
+select id
+from orders 
+where gloss_qty > 4000 or poster_qty > 4000;
+
+select id
+from orders
+where standard_qty = 0 AND (gloss_qty > 1000 or poster_qty > 1000);
+
+SELECT *
+FROM accounts
+WHERE (name LIKE 'C%' OR name LIKE 'W%') 
+              AND ((primary_poc LIKE '%ana%' OR primary_poc LIKE '%Ana%') 
+              AND primary_poc NOT LIKE '%eana%');
+
+-- order of commands does matter: select, from, where, order by, limit
+SELECT col1, col2
+FROM table1
+WHERE col3  > 5 AND col4 LIKE '%os%'
+ORDER BY col5
+LIMIT 10;
+
+--JOINS: allow us to pull data from more than one table at a time.
+--The FROM clause indicates the first table from which we're pulling data, and the JOIN indicates the second table. The ON* clause specifies the column on which you'd like to merge the two tables together.
+--pull all the information from only the orders table:
+SELECT orders.*
+FROM orders
+JOIN accounts
+ON orders.account_id = accounts.id;
+
+--pulls all the columns from both the accounts and orders table.
+SELECT *
+FROM orders
+JOIN accounts
+ON orders.account_id = accounts.id;
+
+--to only pull individual elements from either the orders or accounts table, we can do this by using the exact same information in the FROM and ON statements
+  --The table name is always before the period.
+  --The column you want from that table is always after the period.
+--For example, if we want to pull only the account name and the dates in which that account placed an order, but none of the other columns, we can do this with the following query:
+SELECT accounts.name, orders.occurred_at
+FROM orders
+JOIN accounts
+ON orders.account_id = accounts.id;
+
+SELECT orders.*, accounts.*
+FROM accounts
+JOIN orders
+ON accounts.id = orders.account_id;
+
+SELECT orders.standard_qty, orders.gloss_qty, 
+          orders.poster_qty,  accounts.website, 
+          accounts.primary_poc
+FROM orders
+JOIN accounts
+ON orders.account_id = accounts.id
+
+--primary key: is a unique column in a particular table
+--foreign key: is a column in one table that is a primary key in a different table
+
+--alias
+FROM tablename t1
+JOIN tablename2 t2
